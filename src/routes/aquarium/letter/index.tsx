6d4c54484f93c737ee3a7dee87a750966550463e
Kunori47 <yuualph47@gmail.com>
2025-04-02 19:17:59 -0400

    Fix link path in LetterComponent to use template literal for letter ID

--- src/routes/aquarium/letter/index.tsx
+++ src/routes/aquarium/letter/index.tsx
@@ -34,7 +34,7 @@ function LetterComponent() {
         <LayoutAquarium></LayoutAquarium>
         {
             aquarium && aquarium.map((item) => (
-                <Link to={`/aquarium/letter/${item.id}`} className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 ml-8 mt-10" params={{ letterid: item.id }}>
+                <Link to="/aquarium/letter/$letterid" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 ml-8 mt-10" params={{ letterid: item.id }}>
                     <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" src={item.image} alt="" />
                     <div className="flex flex-col justify-between p-4 leading-normal">
                         <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
