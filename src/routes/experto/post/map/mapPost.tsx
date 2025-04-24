192a2e6bdfdb9773650718ecfd7a675a0ee4dcbe
carlos mendez <carlosalcides190304@gmail.com>
2025-04-24 12:53:04 -0400

    add a query to get all forums

--- src/routes/experto/post/map/mapPost.tsx
+++ src/routes/experto/post/map/mapPost.tsx
@@ -1,16 +1,48 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
 import Map from '@/components/map/mapAllForum'
-
+import { supabase } from "@/lib/supabase";
+import { Forun } from '@/components/map/mapAllForum';
+import { useQuery } from '@tanstack/react-query';
 export const Route = createFileRoute('/experto/post/map/mapPost')({
   component: RouteComponent,
 })
 
 //get all post 
+const fetchPost = async () => {
+    const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("category", "expert");
+
+    if (error) {
+        throw new Error(error.message);
+    }
+    return data;
+};
 
 function RouteComponent() {
+
+    const { data: forums, isLoading, error } = useQuery({
+        queryKey: ['posts'],
+        queryFn: fetchPost,
+    });
+    
+
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+    
+    if (error) {    
+        return <div>Error: {error.message}</div>;
+    }
+
+    console.log(forums)
+
+
+
   return (<>
 
-      <section className="w-full px-24 sm:px-5 grid grid-cols-1">
+      <section className="w-4/5 px-24 sm:px-5 grid grid-cols-1">
             <div className="font-custom max-w-screen-lg gap-8">
                 <div className="flex flex-col">
 
@@ -30,7 +62,7 @@ function RouteComponent() {
 
       </section>
       <div className='absolute top-32'>  
-                    <Map category={'experts'}></Map>
+                    <Map category={'experts'} forums={forums}></Map>
       </div> 
   </>) 
 }
