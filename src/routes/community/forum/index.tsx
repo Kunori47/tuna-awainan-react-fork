c42c914a6789719f5ac99d31fc1e2dda58f330ff
Hanner <hhhernandez.18@est.ucab.edu.ve>
2025-07-07 21:03:18 -0400

    Merge branch comments-map-forum

--- src/routes/community/forum/index.tsx
+++ src/routes/community/forum/index.tsx
@@ -47,74 +47,78 @@ function CommunityComponent() {
 							<h2 className="text-3xl font-bold text-center my-8 mb-2">
 								Sección Comunidad
 							</h2>
-							<p className="text-base text-gray-700 dark:text-gray-400">
-								Foros publicados: {forum?.length}
-							</p>
+
+                            <div className="flex flex-col justify-between my-8 sm:flex-row ">
+                                <p className="text-base text-gray-700 dark:text-gray-400">
+                                    Foros publicados: {forum?.length}
+                                </p>
+                                {session ? (
+                                    <Button className="w-1/6 hover:bg-[#087b9b] min-w-fit">
+                                        <Link to={"/community/forum/new"}>Crear un nuevo foro</Link>
+                                    </Button>
+                                ) : null}
+                            </div>
 						</header>
-						{session ? (
-							<Button className="w-1/6 hover:bg-[#087b9b] min-w-fit">
-								<Link to={"/community/forum/new"}>Crear un nuevo foro</Link>
-							</Button>
-						) : null}
-
-                    <div className="card">
-                        <ul className="grid grid-cols-1 gap-8">
-                            {forum &&
-                                forum.map((foro) => (
-                                    <Link 
-                                        key={foro.id}
-                                        to={"/community/forum/$forumid"}
-                                        params={{ forumid: foro.id }}
-                                        className="block hover:scale-[1.01] transition-transform"
-                                    >
-                                        <li className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl hover:border hover:border-gray-200 md:flex-row md:h-72">
-                                            {/* Imagen del articulo */}
-                                            <div className="w-full h-64 md:w-1/2 md:h-auto">
-                                                <img
-                                                    className="object-cover w-full h-full"
-                                                    // src={`https://mlwyobiniqrtpednprrb.supabase.co/storage/v1/object/public/files/${foro.img}`}
-                                                    src={`https://mlwyobiniqrtpednprrb.supabase.co/storage/v1/object/public/files/posts/${foro.img}`}
-                                                    alt={foro.title}
-                                                />
-                                            </div>
-
-                                            {/* Contenido del articulo */}
-                                            <div className="w-full p-6 flex flex-col justify-between md:w-1/2">
-                                                <div>
-                                                    <h3 className="mb-2 text-2xl font-bold text-gray-900 line-clamp-3">
-                                                        {foro.title}
-                                                    </h3>
-                                                    <p className="text-gray-600 line-clamp-4">
-                                                        {foro.content}
-                                                    </p>
+						
+                        <div className="card">
+                            <ul className="grid grid-cols-1 gap-8">
+                                {forum &&
+                                    forum.map((foro) => (
+                                        <Link 
+                                            key={foro.id}
+                                            to={"/community/forum/$forumid"}
+                                            params={{ forumid: foro.id }}
+                                            className="block hover:scale-[1.01] transition-transform"
+                                        >
+                                            <li className="flex flex-col bg-white shadow-md rounded-lg overflow-hidden hover:shadow-xl hover:border hover:border-gray-200 md:flex-row md:h-72">
+                                                {/* Imagen del articulo */}
+                                                <div className="w-full h-64 md:w-1/2 md:h-auto">
+                                                    <img
+                                                        className="object-cover w-full h-full"
+                                                        // src={`https://mlwyobiniqrtpednprrb.supabase.co/storage/v1/object/public/files/${foro.img}`}
+                                                        src={`https://mlwyobiniqrtpednprrb.supabase.co/storage/v1/object/public/files/posts/${foro.img}`}
+                                                        alt={foro.title}
+                                                    />
                                                 </div>
 
-                                                <div className="mt-4 flex items-center gap-3 text-sm text-gray-500">
-                                                    <span className="font-normal text-gray-500">
-                                                        {foro.profiles.username.charAt(0).toUpperCase() + foro.profiles.username.slice(1)}
-                                                    </span>
-                                                    <span className="text-gray-400">·</span>
-                                                    <time dateTime={foro.created_at}>
-                                                        {new Date(foro.created_at).toLocaleDateString("es-ES", {
-                                                            day: "2-digit",
-                                                            month: "short",
-                                                            year: "numeric"
-                                                        })}
-                                                    </time>
+                                                {/* Contenido del articulo */}
+                                                <div className="w-full p-6 flex flex-col justify-between md:w-1/2">
+                                                    <div>
+                                                        <h3 className="mb-2 text-2xl font-bold text-gray-900 line-clamp-3">
+                                                            {foro.title}
+                                                        </h3>
+                                                        <p className="text-gray-600 line-clamp-4">
+                                                            {foro.content}
+                                                        </p>
+                                                    </div>
+
+                                                    <div className="mt-4 flex items-center gap-3 text-sm text-gray-500">
+                                                        <span className="font-normal text-gray-500">
+                                                            {foro.profiles.username.charAt(0).toUpperCase() + foro.profiles.username.slice(1)}
+                                                        </span>
+                                                        <span className="text-gray-400">·</span>
+                                                        <time dateTime={foro.created_at}>
+                                                            {new Date(foro.created_at).toLocaleDateString("es-ES", {
+                                                                day: "2-digit",
+                                                                month: "short",
+                                                                year: "numeric"
+                                                            })}
+                                                        </time>
+                                                    </div>
                                                 </div>
-                                            </div>
-                                        </li>
-                                    </Link>
-                                ))}
-                        </ul>
+                                            </li>
+                                        </Link>
+                                    ))}
+                            </ul>
+                        </div>
                     </div>
                 </div>
-            </div>
             
             <footer className="mt-12 px-5 py-6 text-center text-sm text-gray-500 border-t max-w-screen-lg mx-auto">
                 Tuna awainan no se hace responsable por la información que suban los
                 usuarios a esta página.
             </footer>
         </section>
+		</>
     );
 }
--- src/routes/novedades/index.tsx
+++ src/routes/novedades/index.tsx
@@ -8,12 +8,7 @@ export const Route = createFileRoute('/novedades/')({
 
 function NovedadesComponent() {
   return (
-    <div className="min-h-screen flex flex-col">
-      {/* Top Bar Azul */}
-      <div className="bg-[#0aaccb] pb-16 border-none flex items-center justify-center h-24">
-        <h2 className='text-3xl md:text-4xl font-bold text-white mt-10'>Novedades</h2>
-      </div>
-      
+    <div className="min-h-screen flex flex-col">      
       <Articles></Articles>
       <Expert></Expert>
     
